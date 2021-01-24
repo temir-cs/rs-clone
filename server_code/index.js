2f50ae153b940e498f854777104edf9df9b6e34c
ilyasbulat <i.alkeev@gmail.com>
2021-01-24 22:13:27 +0600

    feat: send stats to server

--- server_code/index.js
+++ server_code/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const db = require("./db/users");
-const cors = require("cors");
+const bodyParser = require('body-parser');
+const db = require('./db/utils');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
@@ -10,47 +10,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/signup", async (req, res) => {
+app.post('/signup', async (req, res) => {
   let results;
   try {
     results = await db.createUser(req.body);
   } catch (e) {
-    res.status(201).json({ status: "no" });
+    res.status(201).json({ status: 'no' });
   }
   if (results) {
-    res.status(201).json({ status: "ok" });
+    res.status(201).json({ status: 'ok' });
   }
 });
 
-app.post("/signin", async (req, res) => {
+app.post('/signin', async (req, res) => {
   let user;
   try {
     user = await db.getUser(req.body);
   } catch (e) {
     console.log(e);
-    res.status(201).json({ status: "no" });
+    res.status(201).json({ status: 'no' });
   }
   if (user && user.length) {
-    res.status(201).json({ status: "ok" });
+    res.status(201).json({ status: 'ok' });
   } else {
-    res.status(201).json({ status: "no" });
+    res.status(201).json({ status: 'no' });
   }
 });
 
-app.get("/leaderboard", async (req, res) => {
+app.get('/leaderboard', async (req, res) => {
   const scores = await db.getScores();
   res.status(200).json(scores);
 });
 
-// app.patch('/users/:', async (req, res) => {
-//   const id = await db.updateUser(req.param.id, req.body);
-//   res.status(200).json({ id });
-// });
-
-// app.get('/', (req, res) => {
-//   res.send('Hello world');
-// });
+app.post('/leaderboard', async (req, res) => {
+  const scores = await db.setScore(req.body);
+  res.status(200).json(scores);
+});
 
 app.listen(port, () => {
-  console.log("Server is started at port " + port);
+  console.log('Server is started at port ' + port);
 });
