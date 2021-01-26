9b9201a9559a225d618b76da590c6c911b95af73
aitzhans <aitzhan@mail.ru>
2021-01-26 22:59:34 +0600

    refactor: refactor not-shooting enemy as a class

---
+++ src/js/components/entities/EnemyNotShooting.ts
@@ -0,0 +1,46 @@
+import Enemy from './Enemy';
+import Player from './Player';
+import Projectile from '../attacks/Projectile';
+import MeleeWeapon from '../attacks/MeleeWeapon';
+
+class EnemyNotShooting extends Enemy {
+  isDead: boolean;
+  enemyName: string;
+
+  constructor(scene:Phaser.Scene, x:number, y:number, enemyName: string, player: Player) {
+    super(scene, x, y, enemyName, player);
+    this.enemyName = enemyName;
+    this.isDead = false;
+  }
+
+  update(time:number, delta:number):void {
+    super.update(time, delta);
+    if (!this.active) return;
+    if (this.isPlayingAnims(`${this.enemyName}-hurt`) || this.isPlayingAnims(`${this.enemyName}-death`)) { return; }
+    if (this.isDead) {
+      this.setActive(false);
+      this.play(`${this.enemyName}-death`, true);
+      setTimeout(() => {
+        this.rayGraphics.clear();
+        this.destroy();
+      }, 400);
+      return;
+    }
+    this.setVelocityX(this.speed);
+    this.play(`${this.enemyName}-walk`, true);
+  }
+
+  takesHit(source: Projectile | MeleeWeapon):void {
+    super.takesHit(source);
+    this.setVelocityX(this.speed * 0.1);
+    this.play(`${this.enemyName}-hurt`, true);
+
+    if (this.health <= 0) {
+      this.play(`${this.enemyName}-death`, true);
+      this.setVelocityX(0);
+      this.isDead = true;
+    }
+  }
+}
+
+export default EnemyNotShooting;
